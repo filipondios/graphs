13ed8b7804a9e9ffe59d2bf23cd5e7a8ebdbdc6f
filipondios <danialhamilla262@gmail.com>
2025-10-20 19:38:04 +0200

    put headers in include

---
+++ include/hash_map.h
@@ -0,0 +1,23 @@
+#ifndef _HASH_MAP_H_
+#define _HASH_MAP_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum { ERR_INPUT, ERR_ALLOC, ERR_NONE };
+
+typedef struct hash_map hash_map;
+struct hash_map { size_t capacity; size_t count; void* entries; };
+
+hash_map* map_create(size_t entry_size, size_t capacity);
+void map_destroy(hash_map* map);
+void* map_get_or_create_entry(hash_map* map, size_t key, size_t entry_size);
+void* map_get_entry(hash_map* map, size_t key, size_t entry_size);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
