f45dd34f60dfec9c1f8b63951663ad5e1822e54c
filipondios <danialhamilla262@gmail.com>
2025-10-23 17:43:42 +0200

    implement first versions of graph_add_edge

--- src/graph.c
+++ src/graph.c
@@ -1,18 +1,13 @@
-#include <stddef.h>
-#include <stdlib.h>
+#include "hash_map.h"
 #include <graph.h>
 #include <errors.h>
 
 int graph_read_edges(hash_map* map, const edge* edges, size_t num_edges) {
-    if (!map) { return ERR_INPUT; }
-    int result;
-
     for (size_t i = 0; i < num_edges; ++i) {
-        // calling add_connection should add
-        // the edges (u -> v) & (v -> u)
         const edge* e = &edges[i];
-        result = graph_add_connection(map, e->u, e->v, e->w);
-        if (result == ERR_ALLOC) { return ERR_ALLOC; }
+        const int ruv = graph_add_connection(map, e->u, e->v, e->w);
+        const int rvu = graph_add_connection(map, e->v, e->u, e->w);
+        if (ruv == ERR_ALLOC || rvu == ERR_ALLOC) { return ERR_ALLOC; }
     }
     return ERR_NONE;
 }
@@ -22,7 +17,20 @@ int graph_load_edges(hash_map* map, const char* file_path) {
 }
 
 int graph_add_connection(hash_map* graph, size_t u, size_t v, size_t w) {
-    if (!graph) { return ERR_INPUT; }
-    // TODO use map_get_or_create
+    src_entry* source_entry = map_get_or_create_entry(graph, u,
+        sizeof(src_entry), sizeof(hash_map*));
+    if (!source_entry) { return ERR_ALLOC;  }
+
+    if (!source_entry->value) {
+        // If the entry was just created, initialize neighbours
+        source_entry->value = map_create(sizeof(dst_entry), 0);
+        if (!source_entry->value) { return ERR_ALLOC; }
+    }
+
+    dst_entry* neighbours_entry = map_get_or_create_entry(source_entry->value,
+        v, sizeof(dst_entry), sizeof(size_t));
+    if (!neighbours_entry) { return ERR_ALLOC; }
+
+    neighbours_entry->value = w;
     return ERR_NONE;
 }
